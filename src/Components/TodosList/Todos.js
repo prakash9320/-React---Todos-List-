52aee776f0d33dc4d17f13ca8d2173b65fe327da
prakash9320 <prakashshukla275@gmail.com>
2021-10-07 14:07:28 +0530

    add some code

--- src/Components/TodosList/Todos.js
+++ src/Components/TodosList/Todos.js
@@ -1,15 +1,15 @@
-import React from 'react'
 import './Style.css';
+import Logo from '../imges/todo.svg';
 const Todos = () => {
     return (
         <div>
             <div className ="main-div">
                 <div className ="child-div">
                     <figure>
-                        <img src ="" alt ="todologo"/>
+                        <img src={Logo} alt ="todologo"/>
                         <figcaption>Add Your List Here</figcaption>
                         <div className ="addItems">
-                            <input type="text" placeholder="Add Items.." className="form-control"/> clas
+                            <input type="text" placeholder="Add Items.." className="form-control"></input>
                         </div>
                     </figure>
                 </div>
