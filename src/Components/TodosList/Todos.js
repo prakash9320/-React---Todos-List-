ff1d0ee80d51bc6a0b494ac782277a606ad7a59a
prakash9320 <prakashshukla275@gmail.com>
2021-10-07 12:55:06 +0530

    add some css and and components

---
+++ src/Components/TodosList/Todos.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Style.css';
+const Todos = () => {
+    return (
+        <div>
+            <div className ="main-div">
+                <div className ="child-div">
+                    <figure>
+                        <img src ="" alt ="todologo"/>
+                        <figcaption>Add Your List Here</figcaption>
+                        <div className ="addItems">
+                            <input type="text" placeholder="Add Items.." className="form-control"/> clas
+                        </div>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Todos
